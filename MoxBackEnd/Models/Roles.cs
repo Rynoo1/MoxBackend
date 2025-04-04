d82aa5f203378f5ee8b7a3ee831ef945b2ced073
Rynoo1 <113459210+Rynoo1@users.noreply.github.com>
2025-04-04 12:28:25 +0200

    Update Roles.cs

--- MoxBackEnd/Models/Roles.cs
+++ MoxBackEnd/Models/Roles.cs
@@ -1,12 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MoxBackEnd.Models;
 
 public class Roles
 {
-    public string RoleID { get; set; } // PK
-    public string RoleName { get; set; }
-    public string RoleDescription { get; set; }
-    public ICollection<User> Users { get; set; } = new List<User>();
-    public ICollection<Projects> Projects { get; set; } = new List<Projects>();
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int RoleID { get; set; } // PK
+
+
+    public string RoleName { get; set; } = string.Empty;
+    public string RoleDescription { get; set; } 
+
+
+
+    //TODO - Navigation properties - ICollection does not exist
+    // public ICollection<User> Users { get; set; } = new List<User>();
+    // public ICollection<Projects> Projects { get; set; } = new List<Projects>();
 }
