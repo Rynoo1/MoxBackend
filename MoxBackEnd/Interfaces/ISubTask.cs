d7318b388697f9c4f9518ab0ba428f6815e2db51
PHILLIP8264 <113760278+PHILLIP8264@users.noreply.github.com>
2025-04-06 12:19:28 +0200

    interfaces

--- MoxBackEnd/Interfaces/ISubTask.cs
+++ MoxBackEnd/Interfaces/ISubTask.cs
@@ -4,13 +4,19 @@ namespace MoxBackEnd.Interfaces;
 
 public interface ISubTask
 {
-    // int TaskID { get; set; }
-    // string Title { get; set; }
-    // string? Description { get; set; }
-    // DateTime DueDate { get; set; }
-    // bool IsCompleted { get; set; }
-    // DateTime? CompletedDate { get; set; }
-    // int? AssignedUserID { get; set; }
-    // int Priority { get; set; }
-    // int? ParentTaskId { get; set; }
+    public Task<bool> CreateSubTask(SubTasks subTask);
+    public Task<bool> UpdateSubTask(SubTasks subTask);
+    public Task<bool> DeleteSubTask(int subTaskId);
+    public Task<SubTasks?> GetSubTaskById(int subTaskId);
+    public Task<List<SubTasks>> GetAllSubTasksByTaskId(int taskId);
+    public Task<List<SubTasks>> GetAllSubTasksByUserId(int userId);
+    public Task<List<SubTasks>> GetAllSubTasksByStatus(bool isCompleted);
+    public Task<List<SubTasks>> GetAllSubTasksByPriority(int priorityLevel);
+    public Task<List<SubTasks>> GetAllSubTasksByDueDate(DateTime dueDate);
+    public Task<List<SubTasks>> GetAllSubTasksByEmergency(bool isEmergency);
+    public Task<List<SubTasks>> GetAllSubTasksByDateRange(DateTime startDate, DateTime endDate);
+    public Task<List<SubTasks>> GetAllSubTasksByTaskIdAndStatus(int taskId, bool isCompleted);
+    public Task<List<SubTasks>> GetAllSubTasksByUserIdAndStatus(int userId, bool isCompleted);
+    public Task<List<SubTasks>> GetAllSubTasksByTaskIdAndUserId(int taskId, int userId);
+    public Task<List<SubTasks>> GetAllSubTasksByTaskIdAndUserIdAndStatus(int taskId, int userId, bool isCompleted);
 }
