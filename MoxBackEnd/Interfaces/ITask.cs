450b6f4dc504f742ff7086a4c82ece8369d89482
PHILLIP8264 <113760278+PHILLIP8264@users.noreply.github.com>
2025-04-06 12:19:28 +0200

    interfaces

--- MoxBackEnd/Interfaces/ITask.cs
+++ MoxBackEnd/Interfaces/ITask.cs
@@ -6,9 +6,17 @@ namespace MoxBackEnd.Interfaces;
 
 public interface ITask
 {
-    int Id { get; set; }
-    string Title { get; set; }
-    string Description { get; set; }
-    DateTime DueDate { get; set; }
-    int ProjectId { get; set; }
+    public Task<bool> CreateTask(Tasks task);
+    public Task<bool> UpdateTask(Tasks task);
+    public Task<bool> DeleteTask(int taskId);
+    public Task<Tasks?> GetTaskById(int taskId);
+    public Task<List<Tasks>> GetAllTasksByProjectId(int projectId);
+    public Task<List<Tasks>> GetAllTasksByUserId(int userId);
+    public Task<List<Tasks>> GetAllTasksByStatus(bool isCompleted);
+    public Task<List<Tasks>> GetAllTasksByPriority(int priorityLevel);
+    public Task<List<Tasks>> GetAllTasksByDueDate(DateTime dueDate);
+    public Task<List<Tasks>> GetAllTasksByEmergency(bool isEmergency);
+    public Task<List<Tasks>> GetAllTasksByDateRange(DateTime startDate, DateTime endDate);
+    public Task<List<Tasks>> GetAllTasksByProjectIdAndStatus(int projectId, bool isCompleted);
+    public Task<List<Tasks>> GetAllTasksByUserIdAndStatus(int userId, bool isCompleted);
 }
