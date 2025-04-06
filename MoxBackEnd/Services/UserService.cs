d529a96b80cca73fc6cdceba3292ff92775c48f9
PHILLIP8264 <113760278+PHILLIP8264@users.noreply.github.com>
2025-04-06 12:01:11 +0200

    models

--- MoxBackEnd/Services/UserService.cs
+++ MoxBackEnd/Services/UserService.cs
@@ -1,61 +1,19 @@
 using System;
-using MoxBackEnd.Data;
 using MoxBackEnd.Interfaces;
-using MoxBackEnd.Models;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace MoxBackEnd.Services;
 
 public class UserService : IUser
 {
-    private readonly AppDbContext _context;
-    public UserService(AppDbContext context)
-    {
-        _context = context;
-    }
+    public string UserName { get; set; }
+    public string UserID { get; set; }
+    public string UEmail { get; set; }
+    public string UPassword { get; set; }
+    public string ComfirmPassword { get; set; }
+    public string UFirstName { get; set; }
+    public string ULastName { get; set; }
+    public string UPhoneNumber { get; set; }
+    public string ProfileImage { get; set; }
 
-    public Task<Users> GetUserID(int userID)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Roles> GetUserRoles(int userID)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<SubTasks> GetUserSubtasks(int userID)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Tasks> GetUserTasks(int userID)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<string> HashPassword(string password)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<bool> LoginUser(string email, string password)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<bool> RegisterUser(Users user)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Users?> UserExists(string email)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<bool> VaildatePassword(Users user, string password)
-    {
-        throw new NotImplementedException();
-    }
 }
