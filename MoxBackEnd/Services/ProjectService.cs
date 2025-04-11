a38c951dc042ae42abcf90aabd5eca68ffc38530
PHILLIP8264 <113760278+PHILLIP8264@users.noreply.github.com>
2025-04-11 21:01:40 +0200

    project servecis

--- MoxBackEnd/Services/ProjectService.cs
+++ MoxBackEnd/Services/ProjectService.cs
@@ -1,26 +1,52 @@
 using System;
+using System.Threading.Tasks;
 using MoxBackEnd.Data;
 using MoxBackEnd.Models;
 using MoxBackEnd.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace MoxBackEnd.Services
 {
     public class ProjectService : IProjects
     {
         private readonly AppDbContext _context;
+
         public ProjectService(AppDbContext context)
         {
             _context = context;
         }
 
-        public Task<Projects> AddUserAsync(Users user)
+        public async Task<Projects> AddUserAsync(Users user)
         {
-            throw new NotImplementedException();
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            // Add the user to the database
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            // you want to return the user's associated project
+            var project = await _context.Projects
+                .Include(p => p.Users)
+                .FirstOrDefaultAsync(p => p.Users.Contains(user));
+
+            return project;
         }
 
-        public Task<Projects> CreateProjectAsync(Projects projects)
+        public async Task<Projects> CreateProjectAsync(Projects project)
         {
-            throw new NotImplementedException();
+            if (project == null)
+            {
+                throw new ArgumentNullException(nameof(project));
+            }
+
+            // Add the project to the database
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+
+            return project;
         }
     }
 }
