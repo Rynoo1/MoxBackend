b05d2a160020e44833676930fd2a463788a56082
MichaelaKemp <125548449+MichaelaKemp@users.noreply.github.com>
2025-04-15 09:28:00 +0200

    Merged migrations folder
    Merged migrations folder with my changes to try and fix some issues that emerged

--- MoxBackEnd/Controllers/ProjectController.cs
+++ MoxBackEnd/Controllers/ProjectController.cs
@@ -1,72 +1,73 @@
-using Microsoft.AspNetCore.Mvc;
-using MoxBackEnd.Models;
-using MoxBackEnd.Services;
+// using Microsoft.AspNetCore.Mvc;
+// using MoxBackEnd.Interfaces;
+// using MoxBackEnd.Models;
+// using MoxBackEnd.Services;
 
-namespace MoxBackEnd.Controllers;
+// namespace MoxBackEnd.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class ProjectController(IProjectService projectService) : ControllerBase
-{
-    private readonly IProjectService _projectService = projectService;
+// [ApiController]
+// [Route("api/[controller]")]
+// public class ProjectController(IProjects projectService) : ControllerBase
+// {
+//     private readonly IProjects _projectService = projectService;
 
-        [HttpGet]
-        public IActionResult GetAllProjects()
-        {
-            var projects = _projectService.GetAllProjects();
-            return Ok(projects);
-        }
+//         [HttpGet]
+//         public IActionResult GetAllProjects()
+//         {
+//             var projects = _projectService.GetAllProjects();
+//             return Ok(projects);
+//         }
 
-        [HttpGet("{id}")]
-        public IActionResult GetProjectById(int id)
-        {
-            var project = _projectService.GetProjectById(id);
-            if (project == null)
-            {
-                return NotFound();
-            }
-            return Ok(project);
-        }
+//         [HttpGet("{id}")]
+//         public IActionResult GetProjectById(int id)
+//         {
+//             var project = _projectService.GetProjectById(id);
+//             if (project == null)
+//             {
+//                 return NotFound();
+//             }
+//             return Ok(project);
+//         }
 
-        [HttpPost]
-        public IActionResult CreateProject([FromBody] Project project)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+//         [HttpPost]
+//         public IActionResult CreateProject([FromBody] Project project)
+//         {
+//             if (!ModelState.IsValid)
+//             {
+//                 return BadRequest(ModelState);
+//             }
 
-            var createdProject = _projectService.CreateProject(project);
-            return CreatedAtAction(nameof(GetProjectById), new { id = createdProject.Id }, createdProject);
-        }
+//             var createdProject = _projectService.CreateProject(project);
+//             return CreatedAtAction(nameof(GetProjectById), new { id = createdProject.Id }, createdProject);
+//         }
 
-        [HttpPut("{id}")]
-        public IActionResult UpdateProject(int id, [FromBody] Project project)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+//         [HttpPut("{id}")]
+//         public IActionResult UpdateProject(int id, [FromBody] Project project)
+//         {
+//             if (!ModelState.IsValid)
+//             {
+//                 return BadRequest(ModelState);
+//             }
 
-            var updatedProject = _projectService.UpdateProject(id, project);
-            if (updatedProject == null)
-            {
-                return NotFound();
-            }
+//             var updatedProject = _projectService.UpdateProject(id, project);
+//             if (updatedProject == null)
+//             {
+//                 return NotFound();
+//             }
 
-            return Ok(updatedProject);
-        }
+//             return Ok(updatedProject);
+//         }
 
-        [HttpDelete("{id}")]
-        public IActionResult DeleteProject(int id)
-        {
-            var deleted = _projectService.DeleteProject(id);
-            if (!deleted)
-            {
-                return NotFound();
-            }
+//         [HttpDelete("{id}")]
+//         public IActionResult DeleteProject(int id)
+//         {
+//             var deleted = _projectService.DeleteProject(id);
+//             if (!deleted)
+//             {
+//                 return NotFound();
+//             }
 
-            return NoContent();
-        }
-}
+//             return NoContent();
+//         }
+// }
 
---
+++ MoxBackEnd/Migrations/20250414102813_AddingTables.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoxBackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectsID",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ProjectsID",
+                table: "AspNetUsers",
+                column: "ProjectsID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Projects_ProjectsID",
+                table: "AspNetUsers",
+                column: "ProjectsID",
+                principalTable: "Projects",
+                principalColumn: "ProjectsID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Projects_ProjectsID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ProjectsID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectsID",
+                table: "AspNetUsers");
+        }
+    }
+}
