52b0fa771963293e721b28f277ed5a0745885c5f
Tebogo Ramolobeng <221244@virtualwindow.co.za>
2025-05-14 09:47:23 +0200

    feat(sidebar): integrate Bootstrap layout with page links and theme toggle
    Linked sidebar to Home, Kanban, and Projects pages
    Added light/dark mode toggle using localStorage

--- MoxFrontend/src/renderer/src/App.tsx
+++ MoxFrontend/src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
-// src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import KanbanBoard from "./pages/KanbanBoard";
+import Projects from "./pages/Projects";
 import Sidebar from "./components/Sidebar";
 
 function App() {
@@ -17,6 +17,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/kanban" element={<KanbanBoard />} />
+               <Route path="/projects" element={<Projects />} />
+
           </Routes>
         </div>
       </div>
---
+++ MoxFrontend/src/renderer/src/pages/Projects.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Projects: React.FC = () => {
+  return (
+    <div>
+      <h1>Projects Page</h1>
+      <p>manage your projects.</p>
+    </div>
+  );
+};
+
+export default Projects;
