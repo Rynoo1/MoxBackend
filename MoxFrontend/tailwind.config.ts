7af08b1233117f1e7688fdc577c535185f454f72
Tebogo Ramolobeng <221244@virtualwindow.co.za>
2025-05-18 03:14:26 +0200

    update tailwind
    Fix tailwind configuration: update to use defineConfig

---
+++ MoxFrontend/tailwind.config.ts
@@ -0,0 +1,12 @@
+import daisyui from 'daisyui'
+
+const config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,html}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [daisyui],
+  darkMode: 'class'
+}
+
+export default config
